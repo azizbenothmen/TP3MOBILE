9c0b2640dd1972b5a0db31d124a9d530a0f83d57
azizbenothmen <aziz.benothmen@enicar.ucar.tn>
2026-02-24 13:29:27 +0100

    v1

--- app/src/main/java/com/example/two/MainActivity.java
+++ app/src/main/java/com/example/two/MainActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,12 +18,18 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG=MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE="key.for.extra.MESSAGE";
     private EditText mMessageEditText;
+    public static final int TEXT_REQUEST=1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        mMessageEditText=findViewById(R.id.editText_main);
+        mReplyHeadTextView=findViewById(R.id.text_header_reply);
+        mReplyTextView=findViewById(R.id.text_message_reply);
+
         EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -35,12 +43,26 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG,"Button Clicked");
        Log.d(LOG_TAG,mMessageEditText.toString());
 
+
         Intent intent=new Intent(this,SecondActivity.class);
         message=mMessageEditText.getText().toString();
         Log.d(LOG_TAG,message);
         intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
+        startActivityForResult(intent,TEXT_REQUEST);
 
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==TEXT_REQUEST){
+            if(resultCode==RESULT_OK){
+                String reply=data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/two/SecondActivity.java
+++ app/src/main/java/com/example/two/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.two;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY="key.for.extra.REPLY";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class SecondActivity extends AppCompatActivity {
         String message= intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView= findViewById(R.id.text_message);
         textView.setText(message);
+        mReply=findViewById(R.id.editText_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -33,5 +37,13 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
+    }
+
+    public void returnReply(View view) {
+        String reply= mReply.getText().toString();
+        Intent replyIntent= new Intent();
+        replyIntent.putExtra(EXTRA_REPLY,reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
     }
 }
